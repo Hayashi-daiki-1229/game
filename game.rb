cafd8eb43aeb73ec7e71fc755d7b930cd83c4b1e
EC2 Default User <ec2-user@ip-172-31-0-142.ec2.internal>
2022-06-01 07:07:30 +0000

    b

--- game.rb
+++ game.rb
@@ -8,23 +8,22 @@ program_hand = rand(3)
 
 jankens = ["グー","チョキ","パー"]
 puts "あなたの手:#{jankens[player_hand]},相手の手:#{jankens[program_hand]}"
-　
+
 if player_hand == program_hand
  puts "あいこで"
  return true
 elsif (player_hand == 0 && program_hand == 2 ) ||( player_hand == 1 && program_hand == 0 )||( player_hand == 2 && program_hand == 1)
-  puts "あっち向いて"
+  puts "あっち向いて (#{__LINE__}行目)"
   return false
 else 
-  puts "あっち向いて"
+  puts "あっち向いて (#{__LINE__}行目)"
   return false
 end
 end
-
 next_game = true
 
 while next_game
-  next_game = janken
+ next_game = janken
 end
 
 
@@ -35,19 +34,20 @@ def acchimuite_hoi
  program_direction = rand(4)
  directions = ["上","下","左","右"]
   puts "あなた：#{directions[player_direction]} 相手：#{directions[program_direction]}"
+
+if player_direction == program_direction
+    return false
+else
+    return true
+end
 end
 
 
-f1 = janken   #f1には真/偽のいずれかが入る
+f1 = acchimuite_hoi
 
-#あっちむいてホイで二人が同じ方向を向くまで待つ
-loop do
-  f2 = acchimuite_hoi    #f2には真/偽のいずれかが入る
-  break if f2            #二人が同じ方向を向いたらループを出る
-end
 
 if f1
-　puts"あなたの勝ちです！おめでとう"
+puts"あなたの勝ちです！おめでとう"
 else
   #（プレーヤーの）負けの処理
   puts"あなたの負けです・・・残念"
