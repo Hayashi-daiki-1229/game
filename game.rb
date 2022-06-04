9213a282945aeadf6fcb6593cce31113c952da8a
EC2 Default User <ec2-user@ip-172-31-0-142.ec2.internal>
2022-06-04 13:06:20 +0000

    質問

--- game.rb
+++ game.rb
@@ -9,46 +9,51 @@ program_hand = rand(3)
 jankens = ["グー","チョキ","パー"]
 puts "あなたの手:#{jankens[player_hand]},相手の手:#{jankens[program_hand]}"
 
-if player_hand == program_hand
+if player_hand == program_hand then
  puts "あいこで"
  return true
-elsif (player_hand == 0 && program_hand == 2 ) ||( player_hand == 1 && program_hand == 0 )||( player_hand == 2 && program_hand == 1)
+elsif a (player_hand == 0 && program_hand == 2 ) ||( player_hand == 1 && program_hand == 0 )||( player_hand == 2 && program_hand == 1) then
   puts "あっち向いて (#{__LINE__}行目)"
-  return false
-else 
+  return false,result("win")
+elsif b (player_hand == 0 && program_hand == 1 ) ||( player_hand == 1 && program_hand == 2 )||( player_hand == 2 && program_hand == 0) then
   puts "あっち向いて (#{__LINE__}行目)"
-  return false
+  return false,result("lose")
 end
 end
 next_game = true
 
 while next_game
- next_game = janken
+ next_game,result = janken
 end
 
+result("win") = argment(winner) 
+result("lose") = argment(loser)
 
-def acchimuite_hoi
-  #実装する
+def acchimuite_hoi 
   puts "0(上)1(下)2(左)3(右)"
  player_direction = gets.to_i
  program_direction = rand(4)
  directions = ["上","下","左","右"]
   puts "あなた：#{directions[player_direction]} 相手：#{directions[program_direction]}"
 
-if player_direction == program_direction
-    return false
-else
+if player_direction == program_direction 
     return true
+else
+    return false
 end
 end
-
 
 f1 = acchimuite_hoi
 
-
-if f1
+#プレイヤー勝利
+if f1 = atmit(winner)
 puts"あなたの勝ちです！おめでとう"
 else
-  #（プレーヤーの）負けの処理
-  puts"あなたの負けです・・・残念"
+  puts"ジャンケンに戻ります"
+end
+
+if f1 = atmit(loser)
+puts"あなたの負けです"
+else
+  puts"ジャンケンに戻ります"
 end
