f23959e38715a9faf9d620799878d94c78d1886b
EC2 Default User <ec2-user@ip-172-31-0-142.ec2.internal>
2022-10-03 12:05:57 +0000

    勝敗の修正

--- game.rb
+++ game.rb
@@ -14,21 +14,22 @@ class JankenAcchimuite
   player_hand = gets.to_i
   if player_hand < 0 || 2 < player_hand then
   puts "無効な値です"
-  return true
   end
   program_hand = rand(3)
 
   jankens = ["グー","チョキ","パー"]
   puts "あなたの手:#{jankens[player_hand]},相手の手:#{jankens[program_hand]}"
 
-  if player_hand == program_hand
+  if player_hand == program_hand 
    puts "#{@draw}"
    return true
   elsif  (player_hand == 0 && program_hand == 2 ) ||( player_hand == 1 && program_hand == 0 )||( player_hand == 2 && program_hand == 1)
    puts "#{@lose}"
+   $janken_result = "lose"
    return false
   elsif  (player_hand == 0 && program_hand == 1 ) ||( player_hand == 1 && program_hand == 2 )||( player_hand == 2 && program_hand == 0)
    puts "#{@win}"
+   $janken_result = "win"
    return false
   end
  end
@@ -39,31 +40,37 @@ class JankenAcchimuite
   program_direction = rand(4)
   directions = ["上","下","左","右"]
    puts "あなた：#{directions[player_direction]} 相手：#{directions[program_direction]}"
-
-  if player_direction == program_direction
-     if @win
-        puts "#{@win}"
-     elsif @lose
-        puts "#{@lose}"
-     end
-     return false
+  if player_direction < 0 || 4 < player_direction then
+  puts "無効な値です"
+  end
+  
+  if $janken_result == "lose" && player_direction == program_direction
+   puts "#{@lose}"
+    return false
+  end
+  if $janken_result == "win" && player_direction == program_direction
+   puts "#{@win}"
+    return false
   end
   if player_direction != program_direction
    puts "#{@draw}"
+   return true
   end
-  return true
  end
+end
  
  
 while 
  result1 = JankenAcchimuite.new("あいこ","あっち向いて","あっち向いて")
  next_game = true
   while next_game
-  next_game = result1.janken
+   next_game = result1.janken
   end
  result2 = JankenAcchimuite.new("もう一度ジャンケンを行います","負けました","勝ちました")
- if result2.acchimuite_hoi == false
+  if $janken_result == "win" && result2.acchimuite_hoi == false
   break
- end
-end
+  elsif $janken_result == "lose" && result2.acchimuite_hoi == false
+  break
+  else true
+  end
 end
